f904b7ced6d4424d5fc73f5e81cc0a60909b334c
Emanuel Massafera <emanuel301@live.com>
2023-03-20 16:10:00 -0300

    feat: add findByUserIdOnDate method

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,5 +1,6 @@
-import { Prisma, CheckIn } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
+import { Prisma, CheckIn } from '@prisma/client'
+import dayjs from 'dayjs'
 import { CheckInsRepository } from '../check-ins-repository'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
@@ -17,5 +18,20 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     this.items.push(checkIn)
 
     return checkIn
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDay = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    return checkInOnSameDay || null
   }
 }
