89e5c3243baf5f5eee208445404bb0883b27137b
Emanuel Massafera <emanuel301@live.com>
2023-03-21 09:57:10 -0300

    feat: implement prisma repositories

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
-import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
+import { prisma } from '@/lib/prisma'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
@@ -16,6 +16,16 @@ export class PrismaUsersRepository implements UsersRepository {
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
+    })
+
+    return user
+  }
+
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
     })
 
     return user
