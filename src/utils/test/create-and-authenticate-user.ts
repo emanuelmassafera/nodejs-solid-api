6349b7ffa751b0b20205d8af1573b90a8cee6745
Emanuel Massafera <emanuel301@live.com>
2023-03-22 14:12:53 -0300

    feat: add verifyUserRole middleware

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,19 @@
 import request from 'supertest'
 import { FastifyInstance } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'john@mail.com',
-    password: 'any_password',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  role: 'ADMIN' | 'MEMBER' = 'MEMBER',
+) {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john@mail.com',
+      password_hash: await hash('any_password', 6),
+      role,
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
